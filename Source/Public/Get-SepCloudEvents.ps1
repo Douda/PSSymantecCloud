bffd31b728d0dd31d339048da70a694c4b2fcf0e
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2022-11-25 15:45:01 +0100

    added pagination to Get-SepCloudEvents

--- Source/Public/Get-SepCloudEvents.ps1
+++ Source/Public/Get-SepCloudEvents.ps1
@@ -1,4 +1,18 @@
 function Get-SepCloudEvents {
+    <# TODO fill description for Get-SepCloudEvents
+    .SYNOPSIS
+        A short one-line action-based description, e.g. 'Tests if a function is valid'
+    .DESCRIPTION
+        A longer description of the function, its purpose, common use cases, etc.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        Specify a URI to a help page, this will show when Get-Help -Online is used.
+    .EXAMPLE
+        Test-MyTestFunction -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+
     param (
         # file Detection
         [Parameter()]
@@ -8,80 +22,87 @@ function Get-SepCloudEvents {
         # Custom query to run
         [Parameter()]
         [string]
-        $Query,
-
-        # Query date range Past 30 days
-        [Parameter()]
-        [switch]
-        $Past_30_Days,
-
-        # Query date range Past 7 days
-        [Parameter()]
-        [switch]
-        $Past_7_Days
+        $Query
     )
-    
-    # Init
-    $BaseURL = (GetConfigurationPath).BaseUrl
-    $URI_Tokens = 'https://' + $BaseURL + "/v1/event-search"
 
-    # Get token
-    $Token = Get-SEPCloudToken
+    begin {
+        # Init
+        $BaseURL = (GetConfigurationPath).BaseUrl
+        $URI_Tokens = 'https://' + $BaseURL + "/v1/event-search"
 
-    if ($null -ne $Token) {
-        # HTTP body content containing all the queries
-        $Body = @{
-            "product"      = "SAEP"
-            "feature_name" = "ALL"
-        }
-        <#
-        Setting dates for the query
-        Date Format required : -UFormat "%Y-%m-%dT%T.000+00:00" 
-        Example : 
+        # Pagination init
+        $ArrayResponse = @()
+    }
+
+    process {
+        # Get token
+        $Token = Get-SEPCloudToken
+
+        if ($null -ne $Token) {
+            # HTTP body content containing all the queries
+            $Body = @{
+                "product"      = "SAEP"
+                "feature_name" = "ALL"
+            }
+            <#
+        Setting dates for the query Date Format required : -UFormat "%Y-%m-%dT%T.000+00:00"
+        Example :
         "start_date": "2022-10-16T00:00:00.000+00:00",
         "end_date": "2022-11-16T00:00:00.000+00:00"
-        Set correct date formats to query the past 30 days
-        End date = today
-        Start date = X days ago (30 days maximum)
         #>
-        $obj_end_date = Get-Date -AsUTC
-        if ($past_30_Days -eq $true) {
-            $obj_start_date = $obj_end_date.AddDays(-30)
-        }
-        if ($past_7_Days -eq $true) {
-            $obj_start_date = $obj_end_date.AddDays(-7)
-        }
-        $end_date = Get-Date $obj_end_date -UFormat "%Y-%m-%dT%T.000+00:00"
-        $start_date = Get-Date $obj_start_date -UFormat "%Y-%m-%dT%T.000+00:00"
-        $Body.Add("start_date", $start_date)
-        $Body.Add("end_date", $end_date)
 
-        # Iterating through all parameter and adding them to the HTTP body
-        if ($FileDetection -eq $true) {
-            # Testing hardcoded query (file detection)
-            $Body.Add("query", '( feature_name:MALWARE_PROTECTION AND ( type_id:8031 OR type_id:8032 OR type_id:8033 OR type_id:8027 OR type_id:8028 ) AND ( id:12 OR id:11 AND type_id:8031 ) )')
+            $end_date = Get-Date -Format "yyyy-MM-ddTHH:mm:ss.fffK"
+            $start_date = ((Get-Date).addDays(-30) | Get-Date -Format "yyyy-MM-ddTHH:mm:ss.fffK")
+            $Body.Add("start_date", $start_date)
+            $Body.Add("end_date", $end_date)
+
+            # Iterating through all parameter and adding them to the HTTP body
+            switch ($PSBoundParameters.Keys) {
+                'FileDetection' {
+                    $Body.Add("query", '( feature_name:MALWARE_PROTECTION AND ( type_id:8031 OR type_id:8032 OR type_id:8033 OR type_id:8027 OR type_id:8028 ) AND ( id:12 OR id:11 AND type_id:8031 ) )')
+                }
+                'Query' {
+                    $Body.Add("query", "$Query")
+                }
+                Default {
+                }
+            }
+
+            # Setting up pagination
+            $next = 0
+            $limit = 100
+            $Body.Add("limit", $limit)
+            $Body_Json = ConvertTo-Json $Body
+
+            $Headers = @{
+                Host           = $BaseURL
+                Accept         = "application/json"
+                "Content-Type" = "application/json"
+                Authorization  = $Token
+            }
+
+            try {
+                do {
+                    $Response = Invoke-RestMethod -Method POST -Uri $URI_Tokens -Headers $Headers -Body $Body_Json -UseBasicParsing
+                    $ArrayResponse += $Response
+                    $Body.Remove("next")
+                    $Body.Add("next", $next + $limit)
+                    $Body_Json = ConvertTo-Json $Body
+                    # Testing while loop
+                    # $compare_events = ($arrayresponse.events | Measure-Object | Select-Object -ExpandProperty Count)
+                    # $compare_next = ($arrayresponse[0].Total)
+                } while (
+                    <# Compare scraped events vs total from 1st API query #>
+                ($arrayresponse.events | Measure-Object | Select-Object -ExpandProperty Count) -lt ($arrayresponse[0].Total)
+                )
+            } catch {
+                $StatusCode = $_.Exception.Response.StatusCode
+                $StatusCode
+            }
         }
+    }
 
-        if ($Query -ne "") {
-            $Body.Add("query", "$Query")
-        }
-
-
-        $Body_Json = ConvertTo-Json $Body
-
-        $Headers = @{
-            Host           = $BaseURL
-            Accept         = "application/json"
-            "Content-Type" = "application/json"
-            Authorization  = $Token
-        }
-
-        try {
-            $Response = Invoke-RestMethod -Method POST -FollowRelLink -Uri $URI_Tokens -Headers $Headers -Body $Body_Json -UseBasicParsing 
-            return $Response
-        } catch {
-            $StatusCode = $_.Exception.Response.StatusCode
-            $StatusCode
-        }
+    end {
+        return $ArrayResponse
     }
 }
--- Source/Public/Get-SepCloudIncidents.ps1
+++ Source/Public/Get-SepCloudIncidents.ps1
@@ -2,22 +2,31 @@ function Get-SepCloudIncidents {
 
     <# TODO fill description for Get-SepCloudIncidents
     .SYNOPSIS
-        A short one-line action-based description, e.g. 'Tests if a function is valid'
+        Get list of SEP Cloud incidents
     .DESCRIPTION
-        A longer description of the function, its purpose, common use cases, etc.
-    .NOTES
-        Information or caveats about the function e.g. 'This function is not supported in Linux'
-    .LINK
-        Specify a URI to a help page, this will show when Get-Help -Online is used.
+        Get list of SEP Cloud incidents. Using the LUCENE query syntax, you can customize which incidents to gather. More information : https://techdocs.broadcom.com/us/en/symantec-security-software/endpoint-security-and-management/endpoint-security/sescloud/Endpoint-Detection-and-Response/investigation-page-overview-v134374740-d38e87486/Cloud-Database-Search/query-and-filter-operators-by-data-type-v134689952-d38e88796.html
+    .PARAMETER Open
+        filters only opened incidents. Simulates a query "state_id: [0 TO 3]" which represents incidents with the following states <0 Unknown | 1 New | 2 In Progress | 3 On Hold>
+    .PARAMETER Include_events
+        Includes every events that both are part of the context & triggered incident events
+    .PARAMETER Query
+        Type your customer Lucene query to pass to the API
+    .PARAMETER limit
+        Limit of incidents returned by query. max 2000 which is the default
+    .OUTPUTS
+        PSObject containing all SEP incidents
     .EXAMPLE
-        Test-MyTestFunction -Verbose
-        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+        Get-SepCloudIncidents -Open -Include_Events
+    .EXAMPLE
+        Get-SepCloudIncidents -Query "state_id: [0 TO 5]"
+        This query a list of every possible incidents (opened, closed and with "Unknown" status)
     #>
-
 
     param (
         # Opened incidents
-        [Parameter()]
+        [Parameter(
+            ParameterSetName = "QueryDate"
+        )]
         [switch]
         $Open,
 
@@ -27,7 +36,9 @@ function Get-SepCloudIncidents {
         $Include_events,
 
         # Custom query to run
-        [Parameter()]
+        [Parameter(
+            ParameterSetName = "QueryCustom"
+        )]
         [string]
         $Query,
 
@@ -35,17 +46,7 @@ function Get-SepCloudIncidents {
         [Parameter()]
         [ValidateRange(1, 2000)]
         [int]
-        $limit,
-
-        # Query date range Past 30 days
-        [Parameter()]
-        [switch]
-        $Past_30_Days,
-
-        # Query date range Past 7 days
-        [Parameter()]
-        [switch]
-        $Past_7_Days
+        $limit = 2000
     )
 
     # Init
@@ -59,32 +60,30 @@ function Get-SepCloudIncidents {
         # HTTP body content containing all the queries
         $Body = @{}
 
-        # Setting dates format
+        # Settings dates
         $obj_end_date = Get-Date -AsUTC
-        if ($past_30_Days -eq $true) {
-            $obj_start_date = $obj_end_date.AddDays(-30)
-        }
-        if ($past_7_Days -eq $true) {
-            $obj_start_date = $obj_end_date.AddDays(-7)
-        }
+        $obj_start_date = $obj_end_date.AddDays(-29)
         $end_date = Get-Date $obj_end_date -UFormat "%Y-%m-%dT%T.000+00:00"
         $start_date = Get-Date $obj_start_date -UFormat "%Y-%m-%dT%T.000+00:00"
         $Body.Add("start_date", $start_date)
         $Body.Add("end_date", $end_date)
 
-
         # Iterating through all parameter and adding them to the HTTP body
-        if ($Query -ne "") {
-            $Body.Add("query", "$Query")
-        }
-        if ($limit -ne $null) {
-            $Body.Add("limit", $limit)
-        }
-        if ($open -eq $true) {
-            $Body.Add("status", "open")
-        }
-        if ($Include_events -eq $true ) {
-            $Body.Add("include_events", "true")
+        switch ($PSBoundParameters.Keys) {
+            'Query' {
+                $Body.Add("query", "$Query")
+            }
+            'limit' {
+                $Body.Add("limit", $limit)
+            }
+            'Open' {
+                $Body.Add("query", "state_id: [0 TO 3]")
+            }
+            'Include_events' {
+                $Body.Add("include_events", "true")
+            }
+            Default {
+            }
         }
         $Body_Json = ConvertTo-Json $Body
 
@@ -94,7 +93,7 @@ function Get-SepCloudIncidents {
             "Content-Type" = "application/json"
             Authorization  = $Token
         }
-
+        # TODO add pagination like with Get-SepCloudEvents
         try {
             $Response = Invoke-RestMethod -Method POST -Uri $URI_Tokens -Headers $Headers -Body $Body_Json -UseBasicParsing
             return $Response
