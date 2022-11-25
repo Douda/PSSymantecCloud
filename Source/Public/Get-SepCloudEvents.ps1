3cedfbe871df600037c1cbaf5f9adac2eebea4b5
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2022-11-25 18:00:30 +0100

    Adding pagination to Get-SepCloudEvents

--- Source/Public/Get-SepCloudEvents.ps1
+++ Source/Public/Get-SepCloudEvents.ps1
@@ -1,16 +1,17 @@
 function Get-SepCloudEvents {
     <# TODO fill description for Get-SepCloudEvents
     .SYNOPSIS
-        A short one-line action-based description, e.g. 'Tests if a function is valid'
+        Get list of SEP Cloud Events. By default, every events for the past 30 days
     .DESCRIPTION
         A longer description of the function, its purpose, common use cases, etc.
-    .NOTES
-        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .PARAMETER FileDetection
+        Runs the following query under the hood "feature_name:MALWARE_PROTECTION AND ( type_id:8031 OR type_id:8032 OR type_id:8033 OR type_id:8027 OR type_id:8028 ) AND ( id:12 OR id:11 AND type_id:8031 )"
     .LINK
-        Specify a URI to a help page, this will show when Get-Help -Online is used.
+        https://github.com/Douda/PSSymantecCloud
     .EXAMPLE
-        Test-MyTestFunction -Verbose
-        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+        Get-SepCloudEvents -FileDetection
+    .EXAMPLE
+        Get-SepCloudEvents - Query "type_id:8031 OR type_id:8032 OR type_id:8033"
     #>
 
     param (
@@ -29,8 +30,6 @@ function Get-SepCloudEvents {
         # Init
         $BaseURL = (GetConfigurationPath).BaseUrl
         $URI_Tokens = 'https://' + $BaseURL + "/v1/event-search"
-
-        # Pagination init
         $ArrayResponse = @()
     }
 
@@ -39,22 +38,21 @@ function Get-SepCloudEvents {
         $Token = Get-SEPCloudToken
 
         if ($null -ne $Token) {
-            # HTTP body content containing all the queries
+            # HTTP body content containing all mandatory info to start a query
             $Body = @{
                 "product"      = "SAEP"
                 "feature_name" = "ALL"
             }
-            <#
-        Setting dates for the query Date Format required : -UFormat "%Y-%m-%dT%T.000+00:00"
-        Example :
-        "start_date": "2022-10-16T00:00:00.000+00:00",
-        "end_date": "2022-11-16T00:00:00.000+00:00"
-        #>
+            <# Setting dates for the query Date Format required : -UFormat "%Y-%m-%dT%T.000+00:00"
+            Example :
+            "start_date": "2022-10-16T00:00:00.000+00:00",
+            "end_date": "2022-11-16T00:00:00.000+00:00" #>
 
             $end_date = Get-Date -Format "yyyy-MM-ddTHH:mm:ss.fffK"
-            $start_date = ((Get-Date).addDays(-30) | Get-Date -Format "yyyy-MM-ddTHH:mm:ss.fffK")
+            $start_date = ((Get-Date).addDays(-29) | Get-Date -Format "yyyy-MM-ddTHH:mm:ss.fffK")
             $Body.Add("start_date", $start_date)
             $Body.Add("end_date", $end_date)
+            $Body_Json = ConvertTo-Json $Body
 
             # Iterating through all parameter and adding them to the HTTP body
             switch ($PSBoundParameters.Keys) {
@@ -68,12 +66,6 @@ function Get-SepCloudEvents {
                 }
             }
 
-            # Setting up pagination
-            $next = 0
-            $limit = 100
-            $Body.Add("limit", $limit)
-            $Body_Json = ConvertTo-Json $Body
-
             $Headers = @{
                 Host           = $BaseURL
                 Accept         = "application/json"
@@ -82,21 +74,24 @@ function Get-SepCloudEvents {
             }
 
             try {
-                do {
-                    $Response = Invoke-RestMethod -Method POST -Uri $URI_Tokens -Headers $Headers -Body $Body_Json -UseBasicParsing
-                    $ArrayResponse += $Response
-                    $Body.Remove("next")
-                    $Body.Add("next", $next + $limit)
-                    $Body_Json = ConvertTo-Json $Body
-                    # Testing while loop
-                    # $compare_events = ($arrayresponse.events | Measure-Object | Select-Object -ExpandProperty Count)
-                    # $compare_next = ($arrayresponse[0].Total)
-                } while (
-                    <# Compare scraped events vs total from 1st API query #>
-                ($arrayresponse.events | Measure-Object | Select-Object -ExpandProperty Count) -lt ($arrayresponse[0].Total)
-                )
+                $Response = Invoke-RestMethod -Method POST -Uri $URI_Tokens -Headers $Headers -Body $Body_Json -UseBasicParsing
+                $ArrayResponse += $Response
+                if ($null -ne $Response.next) {
+                    <# If pagination #>
+                    do {
+                        # change the "next" offset for next query
+                        $Body.Remove("next")
+                        $Body.Add("next", $Response.next)
+                        $Body_Json = ConvertTo-Json $Body
+                        # Run query & add it to the array
+                        $Response = Invoke-RestMethod -Method POST -Uri $URI_Tokens -Headers $Headers -Body $Body_Json -UseBasicParsing
+                        $ArrayResponse += $Response
+                    } until (
+                        ($null -eq $Response.next)
+                    )
+                }
             } catch {
-                $StatusCode = $_.Exception.Response.StatusCode
+                $StatusCode = $_
                 $StatusCode
             }
         }
