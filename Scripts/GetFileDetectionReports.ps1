3f8e1f8545a453099cb1c2e95c5a68a9083e9c18
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2022-12-03 10:23:58 +0100

    Starting Update-SepCloudAllowList function

--- Scripts/GetFileDetectionReports.ps1
+++ Scripts/GetFileDetectionReports.ps1
@@ -1,57 +1,85 @@
 #TODO finish reporting script
 
 <#
-For monthly detection report : use following event type ID (sources - Symantec ICD Schema - https://icd-schema.symantec.com/types/1)
-Boot Record Detection, 8025
-User Session Detection, 8026
-Process Detection, 8027
-Get-Module Detection, 8028
-Kernel Detection, 8030
-File Detection, 8031
-Registry Key Detection, 8032
-Registry Value Detection, 8033
-
-Below are the values or the "id" property to know the action taken for the detection
-0	Unknown
-1	Blocked
-2	Allowed
-3	No Action
-4	Logged
-5	Command Script Run
-6	Corrected
-7	Partially Corrected
-8	Uncorrected
-10	Delayed	Requires reboot to finish the operation.
-11	Deleted
-12	Quarantined
-13	Restored
-14	Detected
-15	Exonerated	No longer suspicious (re-scored).
-16	Tagged	Marked with extended attributes.
-
+.SYNOPSIS
+    Scripts gathers MALWARE_PROTECTION information from SEP Cloud console.
+    Exports data in a formatted xlsx format
+.DESCRIPTION
+    Runs the following query against the SEP Cloud API 'feature_name:MALWARE_PROTECTION AND type_id:8031'
+.INPUTS
+    -Path required full filepath to export the report
+.OUTPUTS
+    xlsx formatted report
+.NOTES
+    Created by: Aurélien BOUMANNE (02122022)
+.EXAMPLE
+    .\GetFileDetectionReports.ps1 -Path C:\Script\MyMonthlyFileDetectionReport.xlsx
 #>
+
+param (
+    # Path of Export
+    [Parameter(
+        Mandatory
+    )]
+    [string]
+    $Path
+)
+
+# Required modules
 Install-Module -Name PSSymantecCloud
+Install-Module -Name ImportExcel
 
 # Query MALWARE PROTECTION feature (AutoProtect) & File-Detection type
-# Temp query for testing "device_name:WRK201LPE091XHK AND type_id:8031 AND file.name:"BUDGET 2010 RP FC TL _04.12.09.xls""
-# Original query : "feature_name:MALWARE_PROTECTION AND type_id:8031"
-$resp = Get-SepCloudEvents -Query 'device_name:WRK201LPE091XHK AND type_id:8031'
+# Query examples
+# 'feature_name:MALWARE_PROTECTION AND type_id:8031'
+# 'device_name:MachineName AND type_id:8031'
+$resp = Get-SepCloudEvents -Query 'feature_name:MALWARE_PROTECTION AND type_id:8031'
 $array = @()
 foreach ($r in $resp) {
-    <# $r is the current item #>
-    $hashlist = [ordered]@{
+    $Obj = [pscustomobject]@{
         ComputerName      = $r.device_name
-        OperatingSystem   = $r.device_os_name
-        SepGroup          = $r.device_group
-        SepVersion        = $r.product_ver
-        DetectionFile     = $r.file.name
-        DetectionFullPath = $r.file.path
         Time              = $r.time
+        DetectionFile     = $r.file.name
         ThreatName        = $r.threat.name
         Action            = $r.id
+        SepVersion        = $r.product_ver
+        OperatingSystem   = $r.device_os_name
+        SepGroup          = $r.device_group
+        DetectionFullPath = $r.file.path
     }
-
-    $array += $hashlist
+    $array += $Obj
 }
-$array
 
+# Convert ID action value to human readable string
+# Sources : https://icd-schema.symantec.com/type/8031 / Look "disposition"
+$ActionId = @{
+    0  = 'Unknown'
+    1  = 'Blocked'
+    2  = 'Allowed'
+    3  = 'No Action'
+    4  = 'Logged'
+    5  = 'Command Script Run'
+    6  = 'Corrected'
+    7  = 'Partially Corrected'
+    8  = 'Uncorrected'
+    10 = 'Delayed	Requires reboot to finish the operation.'
+    11 = 'Deleted'
+    12 = 'Quarantined'
+    13 = 'Restored'
+    14 = 'Detected'
+    15 = 'Exonerated	No longer suspicious (re-scored).'
+    16 = 'Tagged	Marked with extended attributes.'
+}
+
+# source : https://stackoverflow.com/questions/74645516/replace-psobj-property-value-based-on-a-list
+# parse array
+foreach ($Item in $array) {
+    # parse possible values
+    foreach ($key in $ActionId.Keys) {
+        if ($key -eq $item.Action) {
+            $Item.Action = $ActionId[$key]
+        }
+    }
+}
+
+$array | Export-Excel $Path -WorksheetName "File Detection" -ClearSheet -BoldTopRow -AutoSize -FreezeTopRow -AutoFilter
