36464e5f9d0e8113d6a1e26147d2bab83bfcf54a
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2023-07-26 19:34:22 +0200

    Added Get-SepThreatIntelFileProtection

--- Source/Public/Get-SepCloudDeviceInfo.ps1
+++ Source/Public/Get-SepCloudDeviceInfo.ps1
@@ -1,27 +1,37 @@
 function Get-SepCloudDeviceInfo {
+    #TODO find a way to use the command with the computername and not the device_ID
+    # Remove Device ID as mandatory when adding the computername option
+    [CmdletBinding()]
     param (
         # Mandatory device_ID parameter
-        [Parameter(mandatory)]
+        [Parameter(mandatory,
+            ValueFromPipelineByPropertyName = $true)]
         [string]
         $Device_ID
     )
 
-    # Init
-    $BaseURL = (Get-ConfigurationPath).BaseUrl
-    $URI_Tokens = 'https://' + $BaseURL + "/v1/devices/$Device_ID"
-    # Get token
-    $Token = Get-SEPCloudToken
+    begin {
+        # Init
+        $BaseURL = (Get-ConfigurationPath).BaseUrl
+        $URI_Tokens = 'https://' + $BaseURL + "/v1/devices/$Device_ID"
+        # Get token
+        $Token = Get-SEPCloudToken
+    }
 
-    if ($null -ne $Token) {
-        # HTTP body content containing all the queries
-        $Body = @{}
-        $Headers = @{
-            Host          = $BaseURL
-            Accept        = "application/json"
-            Authorization = $Token
-            Body          = $Body
+    process {
+
+        if ($null -ne $Token) {
+            # HTTP body content containing all the queries
+            $Body = @{}
+            $Headers = @{
+                Host          = $BaseURL
+                Accept        = "application/json"
+                Authorization = $Token
+                Body          = $Body
+            }
+            $Response = Invoke-RestMethod -Method GET -Uri $URI_Tokens -Headers $Headers -Body $Body -UseBasicParsing
+            return $Response
         }
-        $Response = Invoke-RestMethod -Method GET -Uri $URI_Tokens -Headers $Headers -Body $Body -UseBasicParsing
-        return $Response
+
     }
 }
