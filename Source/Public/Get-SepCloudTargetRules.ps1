7fb013e0a8608dc3d071459adbf62ebcd59a00a3
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2023-08-02 13:04:59 +0200

    Merge branch 'develop'

--- Source/Public/Get-SepCloudTargetRules.ps1
+++ Source/Public/Get-SepCloudTargetRules.ps1
@@ -16,6 +16,8 @@ function Get-SepCloudTargetRules {
     }
 
     process {
+        # Another fake comment
+        # And another one
         try {
             $Response = Invoke-RestMethod -Method GET -Uri $URI_Tokens -Headers $Headers -Body $Body -UseBasicParsing
         }
