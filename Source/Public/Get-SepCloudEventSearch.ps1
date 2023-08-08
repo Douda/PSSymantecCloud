a84d5a3a4dcafff36259a9016aa6afae879553b9
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2023-08-08 13:51:09 +0200

    Adding parameters to Get-SepCloudEventSearch & returning object to process block for pipeline support

--- Source/Public/Get-SepCloudEventSearch.ps1
+++ Source/Public/Get-SepCloudEventSearch.ps1
@@ -1,27 +1,44 @@
 function Get-SepCloudEventSearch {
-    <# TODO fill description for Get-SepCloudEventSearch
+    <#
     .SYNOPSIS
-        Get list of SEP Cloud Events. By default, every events for the past 30 days
+        Get list of SEP Cloud Events. By default it will gather data for past 30 days
     .DESCRIPTION
-        A longer description of the function, its purpose, common use cases, etc.
+        Get list of SEP Cloud Events. You can use the following parameters to filter the results: FileDetection, FullScan, or a custom Lucene query
+    .PARAMETER Query
+        Runs a custom Lucene query
+    .PARAMETER FullScan
+        Runs the following query under the hood "Event Type Id:8020-Scan AND Scan Name:Full Scan"
     .PARAMETER FileDetection
         Runs the following query under the hood "feature_name:MALWARE_PROTECTION AND ( type_id:8031 OR type_id:8032 OR type_id:8033 OR type_id:8027 OR type_id:8028 ) AND ( id:12 OR id:11 AND type_id:8031 )"
     .LINK
         https://github.com/Douda/PSSymantecCloud
     .EXAMPLE
-        Get-SepCloudEventSearch -FileDetection
+        Get-SepCloudEventSearch
+        Gather all possible events. ** very slow approach **
     .EXAMPLE
-        Get-SepCloudEventSearch - Query "type_id:8031 OR type_id:8032 OR type_id:8033"
+        Get-SepCloudEventSearch -FileDetection
+        Gather all file detection events
+    .EXAMPLE
+        Get-SepCloudEventSearch -FullScan
+        Gather all full scan events
+    .EXAMPLE
+        Get-SepCloudEventSearch -Query "type_id:8031 OR type_id:8032 OR type_id:8033"
+        Runs a custom Lucene query
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Query')]
     param (
         # file Detection
-        [Parameter()]
+        [Parameter(ParameterSetName = 'FileDetection')]
         [switch]
         $FileDetection,
 
+        # full scan
+        [Parameter(ParameterSetName = 'FullScan')]
+        [switch]
+        $FullScan,
+
         # Custom query to run
-        [Parameter()]
+        [Parameter(ParameterSetName = 'Query')]
         [string]
         $Query
     )
@@ -31,75 +48,72 @@ function Get-SepCloudEventSearch {
         $BaseURL = (Get-ConfigurationPath).BaseUrl
         $URI_Tokens = 'https://' + $BaseURL + "/v1/event-search"
         $ArrayResponse = @()
+        $Token = Get-SEPCloudToken
     }
 
     process {
-        # Get token
-        $Token = Get-SEPCloudToken
 
-        if ($null -ne $Token) {
-            # HTTP body content containing all mandatory info to start a query
-            $Body = @{
-                "product"      = "SAEP"
-                "feature_name" = "ALL"
-            }
-            <# Setting dates for the query Date Format required : -UFormat "%Y-%m-%dT%T.000+00:00"
+        # HTTP body content containing all mandatory info to start a query
+        $Body = @{
+            "product"      = "SAEP"
+            "feature_name" = "ALL"
+        }
+        <# Setting dates for the query Date Format required : -UFormat "%Y-%m-%dT%T.000+00:00"
             Example :
             "start_date": "2022-10-16T00:00:00.000+00:00",
             "end_date": "2022-11-16T00:00:00.000+00:00" #>
 
-            $end_date = Get-Date -Format "yyyy-MM-ddTHH:mm:ss.fffK"
-            $start_date = ((Get-Date).addDays(-29) | Get-Date -Format "yyyy-MM-ddTHH:mm:ss.fffK")
-            $Body.Add("start_date", $start_date)
-            $Body.Add("end_date", $end_date)
+        $end_date = Get-Date -Format "yyyy-MM-ddTHH:mm:ss.fffK"
+        $start_date = ((Get-Date).addDays(-29) | Get-Date -Format "yyyy-MM-ddTHH:mm:ss.fffK")
+        $Body.Add("start_date", $start_date)
+        $Body.Add("end_date", $end_date)
 
-            # Iterating through all parameter and adding them to the HTTP body
-            switch ($PSBoundParameters.Keys) {
-                'FileDetection' {
-                    $Body.Add("query", '( feature_name:MALWARE_PROTECTION AND ( type_id:8031 OR type_id:8032 OR type_id:8033 OR type_id:8027 OR type_id:8028 ) AND ( id:12 OR id:11 AND type_id:8031 ) )')
-                }
-                'Query' {
-                    $Body.Add("query", "$Query")
-                }
-                Default {
-                }
+        # Iterating through all parameter and adding them to the HTTP body
+        switch ($PSBoundParameters.Keys) {
+            'FileDetection' {
+                $Body.Add("query", '( feature_name:MALWARE_PROTECTION AND ( type_id:8031 OR type_id:8032 OR type_id:8033 OR type_id:8027 OR type_id:8028 ) AND ( id:12 OR id:11 AND type_id:8031 ) )')
             }
-
-            # Convert body to Json after adding potential query with parameters
-            $Body_Json = ConvertTo-Json $Body
-
-            $Headers = @{
-                Host           = $BaseURL
-                Accept         = "application/json"
-                "Content-Type" = "application/json"
-                Authorization  = $Token
+            'FullScan' {
+                $Body.Add("query", 'Event Type Id:8020-Scan AND Scan Name:Full Scan')
             }
-
-            try {
-                $Response = Invoke-RestMethod -Method POST -Uri $URI_Tokens -Headers $Headers -Body $Body_Json -UseBasicParsing
-                $ArrayResponse += $Response
-                if ($null -ne $Response.next) {
-                    <# If pagination #>
-                    do {
-                        # change the "next" offset for next query
-                        $Body.Remove("next")
-                        $Body.Add("next", $Response.next)
-                        $Body_Json = ConvertTo-Json $Body
-                        # Run query & add it to the array
-                        $Response = Invoke-RestMethod -Method POST -Uri $URI_Tokens -Headers $Headers -Body $Body_Json -UseBasicParsing
-                        $ArrayResponse += $Response
-                    } until (
-                        ($null -eq $Response.next)
-                    )
-                }
-            } catch {
-                $StatusCode = $_
-                $StatusCode
+            'Query' {
+                $Body.Add("query", "$Query")
             }
+            Default {}
         }
-    }
 
-    end {
+        # Convert body to Json after adding potential query with parameters
+        $Body_Json = ConvertTo-Json $Body
+
+        $Headers = @{
+            Host           = $BaseURL
+            Accept         = "application/json"
+            "Content-Type" = "application/json"
+            Authorization  = $Token
+        }
+
+        try {
+            $Response = Invoke-RestMethod -Method POST -Uri $URI_Tokens -Headers $Headers -Body $Body_Json -UseBasicParsing
+            $ArrayResponse += $Response
+            if ($null -ne $Response.next) {
+                <# If pagination #>
+                do {
+                    # change the "next" offset for next query
+                    $Body.Remove("next")
+                    $Body.Add("next", $Response.next)
+                    $Body_Json = ConvertTo-Json $Body
+                    # Run query & add it to the array
+                    $Response = Invoke-RestMethod -Method POST -Uri $URI_Tokens -Headers $Headers -Body $Body_Json -UseBasicParsing
+                    $ArrayResponse += $Response
+                } until (
+                        ($null -eq $Response.next)
+                )
+            }
+        } catch {
+            $StatusCode = $_
+            $StatusCode
+        }
+
         return $ArrayResponse.events
     }
 }
