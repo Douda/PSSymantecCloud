39af6560b73131937dc5fd76b7e011a161da0bff
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2023-08-25 10:52:03 +0200

    Update pester tests on token

--- Tests/Get-SEPCloudToken.Tests.ps1
+++ Tests/Get-SEPCloudToken.Tests.ps1
@@ -3,43 +3,50 @@ BeforeAll {
     . $PSScriptRoot\..\Source\Private\$ScriptName
 }
 
-# Describe "Get-SEPCloudToken" {
-#     BeforeAll {
+Describe "Get-SEPCloudToken" {
+    Context "With valid ClientID and Secret" {
+        It "Returns a valid token" {
+            $ClientID = "myclientid"
+            $Secret = "mysecret"
+            $Token = Get-SEPCloudToken -ClientID $ClientID -Secret $Secret
+            $Token | Should -Not -BeNullOrEmpty
+        }
+    }
 
-#     }
+    Context "With invalid ClientID and Secret" {
+        It "Throws an error" {
+            $ClientID = "invalidclientid"
+            $Secret = "invalidsecret"
+            { Get-SEPCloudToken -ClientID $ClientID -Secret $Secret } | Should -Throw
+        }
+    }
+}
 
-#     It "returns the correct token" {
-#         $result = Get-SEPCloudToken
-#         $result | Should -BeOfType 'System.String'
-#         $result | Should -StartWith 'Bearer: '
+
+# # Test that the function correctly retrieves a token from the local cred file
+# Describe 'Get-SEPCloudToken' {
+#     Context 'When a valid cred is present in the local cred file' {
+#         # Create a mock cred file
+#         before {
+#             $cred = 'mock-cred'
+#             $cred | Export-Clixml -Path 'C:\temp\mock-cred.xml'
+#         }
+
+#         # Test that the function correctly retrieves the token
+#         It 'Retrieves the token from the local cred file' {
+#             $result = Get-SEPCloudToken -SepCloudCreds 'C:\temp\mock-cred.xml'
+#             $result | Should Be 'mock-token'
+#         }
 #     }
 # }
 
-
-# Test that the function correctly retrieves a token from the local cred file
-Describe 'Get-SEPCloudToken' {
-    Context 'When a valid cred is present in the local cred file' {
-        # Create a mock cred file
-        before {
-            $cred = 'mock-cred'
-            $cred | Export-Clixml -Path 'C:\temp\mock-cred.xml'
-        }
-
-        # Test that the function correctly retrieves the token
-        It 'Retrieves the token from the local cred file' {
-            $result = Get-SEPCloudToken -SepCloudCreds 'C:\temp\mock-cred.xml'
-            $result | Should Be 'mock-token'
-        }
-    }
-}
-
-# Test that the function correctly retrieves a token from the prompt
-Describe 'Get-SEPCloudToken' {
-    Context 'When no valid token or cred is present in local files' {
-        # Test that the function correctly retrieves the token
-        It 'Retrieves the token from the prompt' {
-            $result = Get-SEPCloudToken
-            $result | Should Be 'mock-token'
-        }
-    }
-}
+# # Test that the function correctly retrieves a token from the prompt
+# Describe 'Get-SEPCloudToken' {
+#     Context 'When no valid token or cred is present in local files' {
+#         # Test that the function correctly retrieves the token
+#         It 'Retrieves the token from the prompt' {
+#             $result = Get-SEPCloudToken
+#             $result | Should Be 'mock-token'
+#         }
+#     }
+# }
