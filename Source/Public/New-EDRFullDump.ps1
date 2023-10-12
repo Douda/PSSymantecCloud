a0600227fa1c9f90881f695fe490e603a1267399
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2023-10-12 17:43:54 +0200

    Added description

--- Source/Public/New-EDRFullDump.ps1
+++ Source/Public/New-EDRFullDump.ps1
@@ -1,17 +1,19 @@
 function New-EDRFullDump {
 
-    <# TODO write help
+    <#
     .SYNOPSIS
-        A short one-line action-based description, e.g. 'Tests if a function is valid'
+        Sends a full dump command on an endpoint
     .DESCRIPTION
-        A longer description of the function, its purpose, common use cases, etc.
-    .NOTES
-        Information or caveats about the function e.g. 'This function is not supported in Linux'
-    .LINK
-        Specify a URI to a help page, this will show when Get-Help -Online is used.
+        Sends a full dump command on an endpoint
     .EXAMPLE
-        Test-MyTestFunction -Verbose
-        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+        New-EDRFullDump -ComputerName "computername"
+        Sends a full dump command on an endpoint called "computername"
+    .EXAMPLE
+        New-EDRFullDump -ComputerName "computername" -Description "My description"
+        Sends a full dump command on an endpoint with a description "My description"
+    .EXAMPLE
+        "computername" | New-EDRFullDump
+        Sends a full dump command on an endpoint called "computername" from pipeline
     #>
 
     [CmdletBinding()]
