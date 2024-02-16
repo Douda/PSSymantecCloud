aa78b432817e463bca3ad4b544c24f838dffd09f
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2024-02-16 10:19:30 +0100

    Removed build number for consistent path when dev & test

--- __For_Dev_And_Maintenance/Build_and_load_module.ps1
+++ __For_Dev_And_Maintenance/Build_and_load_module.ps1
@@ -1,9 +1,10 @@
 # Setting up Paths & Variables
 $ModuleDevPath = Split-Path -Path $PSScriptRoot -Parent
 $BuildModuleSourcePath = $ModuleDevPath + "\Source\PSSymantecCloud.psd1"
-$MajorMinorPatch = dotnet-gitversion | ConvertFrom-Json | Select-Object -Expand MajorMinorPatch
+# $MajorMinorPatch = dotnet-gitversion | ConvertFrom-Json | Select-Object -Expand MajorMinorPatch
 $FullSemVer = dotnet-gitversion | ConvertFrom-Json | Select-Object -Expand FullSemVer
-$ImportModulePath = $ModuleDevPath + "\Output\PSSymantecCloud\" + $MajorMinorPatch + "\PSSymantecCloud.psm1"
+# $ImportModulePath = $ModuleDevPath + "\Output\PSSymantecCloud\" + $MajorMinorPatch + "\PSSymantecCloud.psm1"
+$ImportModulePath = $ModuleDevPath + "\Output\PSSymantecCloud\PSSymantecCloud.psm1"
 
 # Build Module
 Build-Module -SourcePath $BuildModuleSourcePath -SemVer $FullSemVer
