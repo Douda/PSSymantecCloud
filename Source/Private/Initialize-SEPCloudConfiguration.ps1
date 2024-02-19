4785bb10f0a2f53f19eeb17daad58775d686c669
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2024-02-19 16:25:47 +0100

    Moved  initialization script for module configuration to private function folder. Updated configuration script & configuration file locations

---
+++ Source/Private/Initialize-SEPCloudConfiguration.ps1
@@ -0,0 +1,73 @@
+####################################
+# Init script for the whole module #
+####################################
+
+## This is the initialization script for the module.  It is invoked at the end of the module's
+## prefix file as "zz_" to load this module at last.  This is done to ensure that all other functions are first loaded
+## This function should be private but will stay Public for the moment as it needs to be the last function to be loaded in the module
+## TODO make this function private
+
+# Update the data types when loading the module
+Update-TypeData -PrependPath (Join-Path -Path $PSScriptRoot -ChildPath 'PSSymantecCloud.Types.ps1xml')
+
+# The credentials used to authenticate to the SES Cloud API
+[string]         $script:Credential = $null # string type used as credentials is OAuth2 token
+[PSCustomObject] $script:accessToken = $null
+
+# The session-cached copy of the module's configuration properties
+[PSCustomObject] $script:configuration = $null
+
+# Module name
+[string] $script:ModuleName = "PSSymantecCloud"
+
+# Load the configuration file
+$script:configuration = [PSCustomObject]@{
+    BaseURL         = "api.sep.securitycloud.symantec.com"
+    SepCloudCreds   = [System.IO.Path]::Combine(
+        [System.Environment]::GetFolderPath('ApplicationData'),
+        'PSSymantecCloud',
+        'creds.xml')
+    CachedTokenPath = [System.IO.Path]::Combine(
+        [System.Environment]::GetFolderPath('LocalApplicationData'),
+        'PSSymantecCloud',
+        'accessToken.xml')
+}
+
+
+function Initialize-SEPCloudConfiguration {
+    <#
+    .SYNOPSIS
+        Populates the configuration of the module for this session, loading in any values
+        that may have been saved to disk.
+
+    .DESCRIPTION
+        Populates the configuration of the module for this session, loading in any values
+        that may have been saved to disk.
+
+    .NOTES
+        Internal helper method.  This is actually invoked at the END of this file.
+    #>
+    [CmdletBinding()]
+    param()
+
+    # Load credential from disk if it exists
+    if (Test-Path -Path $($script:configuration.SepCloudCreds)) {
+        try {
+            $script:Credential = Import-Clixml -Path $($script:configuration.SepCloudCreds)
+        } catch {
+            Write-Verbose "No credentials found from $($script:configuration.SepCloudCreds)"
+        }
+    }
+
+    # Load access token from disk
+    if (Test-Path -Path $($script:configuration.CachedTokenPath)) {
+        try {
+            $script:accessToken = Import-Clixml -Path $($script:configuration.CachedTokenPath)
+        } catch {
+            Write-Verbose "Failed to import access token from $($script:configuration.CachedTokenPath): $_" -Verbose
+        }
+    }
+}
+
+# Invoke the initialization method to populate the configuration
+Initialize-SEPCloudConfiguration
