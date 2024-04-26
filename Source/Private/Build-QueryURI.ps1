12e35e6a5c9af7f56944eec1453f4d094ee56217
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2024-04-26 15:02:42 +0200

    Add Build-QueryURI function to construct a URI with query strings in Build-QueryURI.ps1

---
+++ Source/Private/Build-QueryURI.ps1
@@ -0,0 +1,44 @@
+function Build-QueryURI {
+    <#
+    .SYNOPSIS
+        Constructs a URI from a base URI and query strings
+    .DESCRIPTION
+        Constructs a URI from a base URI and query strings
+    .PARAMETER BaseURI
+        The base URI to use
+    .PARAMETER QueryStrings
+        A hashtable of query strings to add to the URI
+    .NOTES
+        helper function
+    .EXAMPLE
+        $BaseURI = "https://Server01:8446/sepm/api/v1/computers"
+        $QueryStrings = @{
+            sort      = "COMPUTER_NAME"
+            pageIndex = 1
+            pageSize  = 100
+        }
+        $URI = Build-QueryURI -BaseURI $BaseURI -QueryStrings $QueryStrings
+
+        This example will :
+        - Construct a URI from the base URI "https://Server01:8446/sepm/api/v1/computers"
+        - With the query strings "sort=COMPUTER_NAME&pageIndex=1&pageSize=100"
+        resulting in "https://Server01:8446/sepm/api/v1/computers?sort=COMPUTER_NAME&pageIndex=1&pageSize=100"
+    #>
+
+
+    param (
+        [string]$BaseURI,
+        [hashtable]$QueryStrings
+    )
+
+    # Construct the URI
+    $builder = New-Object System.UriBuilder($BaseURI)
+    $query = [System.Web.HttpUtility]::ParseQueryString($builder.Query)
+    foreach ($param in $QueryStrings.GetEnumerator()) {
+        $query[$param.Key] = $param.Value
+    }
+    $builder.Query = $query.ToString()
+    $BaseURI = $builder.ToString()
+
+    return $BaseURI
+}
