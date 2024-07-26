086df391c2a99118305a19ec14f0bbd9f2e64315
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2024-07-26 12:07:02 +0200

    Get-SEPCloudGroup removed listDevices parameter & added API pagination for 1000+ groups environments

--- Source/Public/Get-SEPCloudGroup.ps1
+++ Source/Public/Get-SEPCloudGroup.ps1
@@ -34,14 +34,7 @@ function Get-SEPCloudGroup {
             ParameterSetName = 'DeviceListSet'
         )]
         [String]
-        $GroupID,
-
-        # List devices switch
-        [Parameter(
-            ParameterSetName = 'DeviceListSet'
-        )]
-        [switch]
-        $listDevices
+        $GroupID
     )
 
     begin {
@@ -55,79 +48,82 @@ function Get-SEPCloudGroup {
         # Setting up the URI
         if ($GroupID) {
             $URI = $URI + "/$GroupID"
-
-            if ($listDevices) {
-                $URI = $URI + "/devices"
-            }
         }
 
-        # Setting up the parameters
-        $params = @{
-            Method  = 'GET'
-            Uri     = $uri
-            Headers = @{
-                # Host          = $baseUrl
-                Accept        = "application/json"
-                Authorization = $token
-            }
-        }
+        $allResponse = [PSCustomObject]@{}
+        do {
+            try {
+                # Setting up the parameters
+                $params = @{
+                    Method  = 'GET'
+                    Uri     = $uri
+                    Headers = @{
+                        # Host          = $baseUrl
+                        Accept        = "application/json"
+                        Authorization = $token
+                    }
+                }
 
-        # Invoke the request
-        try {
-            $response = Invoke-ABWebRequest @params
+                # Invoke the request
+                $response = Invoke-ABWebRequest @params
 
-            # if $response is null, return an empty object to avoid further errors
-            if ($null -eq $response) {
-                return [PSCustomObject]@{}
+                # Process the response
+                # If first pass, return the whole response, otherwise append to it
+                if ($queryStrings) {
+                    # Other pass
+                    $allResponse.device_groups += $response.device_groups
+                } else {
+                    # First pass
+                    $allResponse = $response
+                }
+
+                # Setup pagination only for full group list and not for a single group
+                if (!$GroupID) {
+                    # QueryString parameters for pagination
+                    $queryStrings = @{
+                        offset = $response.device_groups.count
+                    }
+                    # Increment the offset if necessary
+                    $uri = Build-QueryURI -BaseURI $uri -QueryStrings $queryStrings
+                }
+
+            } catch {
+                Write-Warning -Message "Error: $_"
             }
-        } catch {
-            "Error : " + $_
-        }
+        } until (
+            $allResponse.device_groups.count -ge $response.total
+        )
+
+
 
         ########################
         # Parsing the response #
         ########################
 
-        # if ListDevices is set, we get a list of devices
-        if ($listDevices) {
-            # If we get a list of devices
-            # Add Device-List PSTypeName to the response
-            $response | ForEach-Object {
-                $_.PSTypeNames.Insert(0, "SEPCloud.Device-List")
-            }
-
-            # Add Device PSTypeName to the devices
-            $response.devices | ForEach-Object {
-                $_.PSTypeNames.Insert(0, "SEPCloud.Device")
-            }
-        }
-
         # If groupID is set, we get a single group
-        elseif ($GroupID) {
+        if ($GroupID) {
             # Add a new property with the full chain of names
-            $groups = (Get-SEPCloudGroup).device_groups
-            $FullNameChain = Get-NameChain -CurrentGroup $response -AllGroups $groups -Chain ""
-            $response | Add-Member -NotePropertyName "fullPathName" -NotePropertyValue $FullNameChain.TrimEnd(" > ")
+            $FullNameChain = Get-NameChain -CurrentGroup $response -AllGroups (Get-SEPCloudGroup).device_groups -Chain ""
+            $allResponse | Add-Member -NotePropertyName "fullPathName" -NotePropertyValue $FullNameChain.TrimEnd(" > ")
 
             # Add PSTypeName to the response
-            $response.PSTypeNames.Insert(0, "SEPCloud.Device-Group")
+            $allResponse.PSTypeNames.Insert(0, "SEPCloud.Device-Group")
 
             # If nothing is set, we get a list of groups
         } else {
             # Add a new property to each group with the full path name (group)
-            $response.device_groups | ForEach-Object {
-                $FullNameChain = Get-NameChain -CurrentGroup $_ -AllGroups $groups.device_groups -Chain ""
+            $allResponse.device_groups | ForEach-Object {
+                $FullNameChain = Get-NameChain -CurrentGroup $_ -AllGroups $allResponse.device_groups -Chain ""
                 $_ | Add-Member -NotePropertyName "fullPathName" -NotePropertyValue $FullNameChain.TrimEnd(" > ")
             }
 
             # Add PSTypeName to the response
-            $response.device_groups | ForEach-Object {
+            $allResponse.device_groups | ForEach-Object {
                 $_.PSTypeNames.Insert(0, "SEPCloud.Device-Group")
             }
         }
 
         # Return the response
-        return $response
-
+        return $allResponse
     }
 }
