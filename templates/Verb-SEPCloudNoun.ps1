8d1dbe14c3a9bda6a8cd97d79d261aa71cb06e69
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2024-08-08 18:53:30 +0200

    update function template

--- templates/Verb-SEPCloudNoun.ps1
+++ templates/Verb-SEPCloudNoun.ps1
@@ -7,14 +7,17 @@ function Verb-SEPCloudNoun {
         .DESCRIPTION
         {required: more detailed description of the function's purpose}
 
-        .NOTES
-        Written by {required}
-        Twitter: {optional}
-        GitHub: {optional}
-        Any other links you'd like here
-
         .LINK
         https://github.com/Douda/PSSymantecCloud
+
+        .PARAMETER Param1
+        {required: description of Param1}
+
+        .PARAMETER Param2
+        {required: description of Param2}
+
+        .PARAMETER Param3
+        {required: description of Param3}
 
         .EXAMPLE
         {required: show one or more examples using the function}
@@ -30,34 +33,31 @@ function Verb-SEPCloudNoun {
     )
 
     begin {
-
-        # The Begin section is used to perform one-time loads of data necessary to carry out the function's purpose
-        # If a command needs to be run with each iteration or pipeline input, place it in the Process section
-
-        # Check to ensure that a session to the SaaS exists and load the required data for authentication
-        Test-SEPCloudConnection
+        # Check to ensure that a session to the SaaS exists and load the needed header data for authentication
+        Test-SEPCloudConnection | Out-Null
 
         # API data references the name of the function
         # For convenience, that name is saved here to $function
         $function = $MyInvocation.MyCommand.Name
 
-        # Retrieve all of the URI, method, body, query, result, filter, and success details for the API endpoint
+        # Retrieve all of the URI, method, body, query, result, and success details for the API endpoint
         Write-Verbose -Message "Gather API Data for $function"
-        $resources = Get-RubrikAPIData -endpoint $function
+        $resources = Get-SEPCLoudAPIData -endpoint $function
         Write-Verbose -Message "Load API data for $($resources.Function)"
         Write-Verbose -Message "Description: $($resources.Description)"
     }
 
     process {
-        # TODO template work in progress
-        $uri = New-URIString -server $Server -endpoint ($resources.URI) -id $id
-        # $body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)
-        $result = Submit-Request -uri $uri -header $Header -method $($resources.Method) -body $body
+        $uri = New-URIString -endpoint ($resources.URI) -id $id
+        $uri = Test-QueryParam -querykeys ($resources.Query.Keys) -parameters ((Get-Command $function).Parameters.Values) -uri $uri
+        $body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)
+
+        Write-Verbose -Message "Body is $(ConvertTo-Json -InputObject $body)"
+        $result = Submit-Request -uri $uri -header $script:SEPCloudConnection.header -method $($resources.Method) -body $body
+
+        $result = Test-ReturnFormat -result $result -location $resources.Result
+        $result = Set-ObjectTypeName -TypeName $resources.ObjectTName -result $result
 
         return $result
-    }
-
-    end {
-
     }
 }
