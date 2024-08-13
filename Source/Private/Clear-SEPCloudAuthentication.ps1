cec965f229d2b3a1162db9db03c7e36071fdb701
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2024-08-13 08:58:10 +0200

    temporarily moving to private function to bypass case insensitive file renaming issue

---
+++ Source/Private/Clear-SEPCloudAuthentication.ps1
@@ -0,0 +1,33 @@
+function Clear-SEPCloudAuthentication {
+    <#
+    .SYNOPSIS
+        Clears out any API token from memory, as well as from local file storage.
+    .DESCRIPTION
+        Clears out any API token from memory, as well as from local file storage.
+    .EXAMPLE
+        Clear-SepCloudAuthentication
+
+        Clears out any API token from memory, as well as from local file storage.
+    #>
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param ()
+
+    # Remove the cached authentication data from memory
+    $script:configuration.CachedToken = $null
+    $script:Credential = $null
+    $script:SEPCloudConnection.AccessToken = $null
+    $script:SEPCloudConnection.Credential = $null
+
+    # remove the cached authentication data from disk
+    Remove-Item -Path $($script:configuration.CachedTokenPath) -Force -ErrorAction SilentlyContinue -ErrorVariable ev
+    Remove-Item -Path $($script:configuration.SEPCloudCredsPath) -Force -ErrorAction SilentlyContinue -ErrorVariable ev
+
+    if (($null -ne $ev) -and
+            ($ev.Count -gt 0) -and
+            ($ev[0].FullyQualifiedErrorId -notlike 'PathNotFound*')) {
+        $message = "Experienced a problem trying to remove the file that persists the Access Token " + $($script:configuration.SEPCloudCredsPath)
+        $message += "Experienced a problem trying to remove the file that persists the Access Credentials " + $($script:configuration.CachedTokenPath)
+        Write-Warning -Message $message
+    }
+}
