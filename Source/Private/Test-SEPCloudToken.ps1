673cb2a09d0f429c03bb18c71b6fc447131e56e7
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2024-08-17 11:11:59 +0200

    switched method to remove in-memory token to avoid SetValueInvocationException when already empty

--- Source/Private/Test-SEPCloudToken.ps1
+++ Source/Private/Test-SEPCloudToken.ps1
@@ -10,8 +10,8 @@ function Test-SEPCloudToken {
         } else {
             Write-Verbose -Message "token expired - deleting in-memory"
             Write-Verbose -Message "token expired - deleting local copy at $($script:configuration.CachedTokenPath)"
-            $script:SEPCloudConnection.AccessToken = $null
-            $script:configuration.AccessToken = $null
+            $script:SEPCloudConnection | Add-Member -MemberType NoteProperty -Name AccessToken -Value $null -Force
+            $script:configuration | Add-Member -MemberType NoteProperty -Name AccessToken  -Value $null -Force
             if ($script:configuration.CachedTokenPath) {
                 Remove-Item $script:configuration.CachedTokenPath -Force
             }
