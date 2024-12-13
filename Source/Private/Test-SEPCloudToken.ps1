24ab4cd77b1590f4c51f158899ae765b13bcdd58
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2024-12-13 12:00:25 +0100

    updated Test-SEPCloudToken logic

--- Source/Private/Test-SEPCloudToken.ps1
+++ Source/Private/Test-SEPCloudToken.ps1
@@ -6,20 +6,22 @@ function Test-SEPCloudToken {
         $token
     )
 
+    # Token passed as parameter
+    if ($token) {
+        if ((Get-Date) -lt $token.Expiration) {
+            return $True
+        }
+    }
+
+    # In memory token
     if ($script:SEPCloudConnection.AccessToken) {
-        # Check if still valid
         if ((Get-Date) -lt $script:SEPCloudConnection.AccessToken.Expiration) {
             return $True
         } else {
             Remove-SEPCloudToken
             return $false
         }
-    } elseif ($token) {
-        if ((Get-Date) -lt $token.Expiration) {
-            return $True
-        }
-    } else {
-        Write-Verbose -Message "no token available in memory"
-        return $False
     }
+
+    return $False
 }
