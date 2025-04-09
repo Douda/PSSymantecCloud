bc88a2b9de2ddcf8b586c14b23bd53fdbd8dfc94
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2025-04-09 17:24:14 +0200

    added manual API parameter to publish module

--- __For_Dev_And_Maintenance/Publish_master_branch_PSGallery.ps1
+++ __For_Dev_And_Maintenance/Publish_master_branch_PSGallery.ps1
@@ -7,8 +7,10 @@ $API_PATH = Split-Path $ModuleDevPath -Parent
 $API_KEY = Import-Clixml -Path "$API_PATH\API_KEY_PS_Gallery.xml" -ErrorAction SilentlyContinue
 if ($null -eq $API_KEY) {
     $API_KEY = Read-Host -Prompt 'Enter PS Gallery API Key to publish the module'
+} else {
+    $API_KEY = Read-Host -Prompt 'Enter PS Gallery API Key to publish the module'
 }
 
 # Publish Module
-$MajorMinorPatch = dotnet-gitversion | ConvertFrom-Json | Select-Object -Expand MajorMinorPatch
-Publish-Module -Path "$ModuleDevPath\Output\PSSymantecCloud\$MajorMinorPatch\" -NuGetApiKey $API_KEY -Verbose
+# $MajorMinorPatch = dotnet-gitversion | ConvertFrom-Json | Select-Object -Expand MajorMinorPatch
+Publish-Module -Path "$ModuleDevPath\Output\PSSymantecCloud\" -NuGetApiKey $API_KEY -Verbose
