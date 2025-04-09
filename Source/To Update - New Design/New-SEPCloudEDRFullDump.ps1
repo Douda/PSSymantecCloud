2a6a984d7909f1ebd3e347b40ed53b6620f957a4
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2025-04-09 17:07:48 +0200

    updated New-SEPCloudEDRFullDump to new design (not public yet)

--- Source/To Update - New Design/New-SEPCloudEDRFullDump.ps1
+++ Source/To Update - New Design/New-SEPCloudEDRFullDump.ps1
@@ -1,70 +1,66 @@
 function New-SEPCloudEDRFullDump {
 
-    <# TODO write help
-    .SYNOPSIS
-        A short one-line action-based description, e.g. 'Tests if a function is valid'
-    .DESCRIPTION
-        A longer description of the function, its purpose, common use cases, etc.
-    .NOTES
-        Information or caveats about the function e.g. 'This function is not supported in Linux'
-    .LINK
-        Specify a URI to a help page, this will show when Get-Help -Online is used.
-    .EXAMPLE
-        Test-MyTestFunction -Verbose
-        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    <#
+        .SYNOPSIS
+            lets you send the full dump command on the device
+        .DESCRIPTION
+            lets you send the full dump command on the device
+            By default the command will have time range of from 29 days to 10 minutes ago
+        .LINK
+            https://github.com/Douda/PSSymantecCloud
+        .PARAMETER device_id
+        device id of the client to send to the command to
     #>
 
     [CmdletBinding()]
-    param (
-        # ComputerName
-        [Parameter(
-            ValueFromPipeline = $true
-        )]
+    Param(
+        [Parameter(Mandatory = $true)]
         [string]
-        [Alias("Hostname", "Computer")]
-        $ComputerName,
+        $device_id,
 
-        # description
+        # [Parameter(Mandatory = $true)]
         [string]
-        $Description
+        $description,
 
+        $from_date = ((Get-Date).AddDays(-29) | Get-Date -Format "yyyy-MM-ddTHH:mm:ss.fffK"), # Default is 29 days ago
+        $to_date = ((Get-Date).AddMinutes(-10) | Get-Date -Format "yyyy-MM-ddTHH:mm:ss.fffK") # Default is 10 minutes ago
     )
 
     begin {
-        # Init
-        $BaseURL = $($script:configuration.BaseURL)
-        $Token = (Get-SEPCloudToken).Token_Bearer
+        # Check to ensure that a session to the SaaS exists and load the needed header data for authentication
+        Test-SEPCloudConnection | Out-Null
+
+        # API data references the name of the function
+        # For convenience, that name is saved here to $function
+        $function = $MyInvocation.MyCommand.Name
+
+        # Retrieve all of the URI, method, body, query, result, and success details for the API endpoint
+        Write-Verbose -Message "Gather API Data for $function"
+        $resources = Get-SEPCLoudAPIData -endpoint $function
+        Write-Verbose -Message "Load API data for $($resources.Function)"
+        Write-Verbose -Message "Description: $($resources.Description)"
     }
 
     process {
-        # Get Computer UID from ComputerName
-        $Device_ID = (Get-SEPCloudDevice -Computername $Computername).id
-        $URI = 'https://' + $BaseURL + "/v1/commands/endpoint-search/fulldump"
+        # Description is a mandatory parameter but is not enforced for convinience
+        # If the description is not provided, then generate a default one based off the computer details
+        if (-not $description) {
+            Write-Information -Message "No description provided for $($resources.Function)"
+            $deviceDetails = Get-SepCloudDeviceDetails -Device_ID $device_id
+            $description = "$($deviceDetails.name) - from $($from_date) - to $($to_date)"
+            Write-Information -Message "Description will be : $($description)"
 
-        if ([string]::IsNullOrEmpty($Description)) {
-            $message = "$ComputerName - Full Dump request"
-            $Description = $message
         }
-        $body = @{
-            device_id   = $Device_ID
-            description = $Description
-        }
+        $uri = New-URIString -endpoint ($resources.URI)
+        $uri = Test-QueryParam -querykeys ($resources.Query.Keys) -parameters ((Get-Command $function).Parameters.Values) -uri $uri
+        $body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)
 
-        $params = @{
-            Uri             = $URI
-            Method          = 'POST'
-            Body            = $body | ConvertTo-Json
-            Headers         = @{
-                Host           = $BaseURL
-                Accept         = "application/json"
-                "Content-Type" = "application/json"
-                Authorization  = $Token
-            }
-            UseBasicParsing = $true
-        }
+        Write-Verbose -Message "Body is $(ConvertTo-Json -InputObject $body)"
+        $result = Submit-Request -uri $uri -header $script:SEPCloudConnection.header -method $($resources.Method) -body $body
 
-        $Resp = Invoke-RestMethod @params
+        $result = Test-ReturnFormat -result $result -location $resources.Result
+        $result = Set-ObjectTypeName -TypeName $resources.ObjectTName -result $result
 
-        return $Resp
+        return $result
     }
 }
