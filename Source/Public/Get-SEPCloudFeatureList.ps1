c48c79c9ec694b6dc0ee7864ca0e48a6a823a3dc
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2025-04-13 10:44:42 +0200

    new design for Get-SepCloudFeatureList

---
+++ Source/Public/Get-SEPCloudFeatureList.ps1
@@ -0,0 +1,50 @@
+function Get-SepCloudFeatureList {
+
+    <#
+    .SYNOPSIS
+        retrieve SES enumeration details for your devices like feature names, security status and reason codes.
+    .DESCRIPTION
+        retrieve SES enumeration details for your devices like feature names, security status and reason codes.
+    .PARAMETER
+        None
+    .INPUTS
+        None
+    .OUTPUTS
+        PSObject
+    .EXAMPLE
+        Get-SepCloudFeatureList
+
+        Gathers all possible feature name, content name, security status and reason codes the API can provide and its related IDs
+    #>
+
+    [CmdletBinding()]
+    Param()
+
+    begin {
+        # Check to ensure that a session to the SaaS exists and load the needed header data for authentication
+        Test-SEPCloudConnection | Out-Null
+
+        # API data references the name of the function
+        # For convenience, that name is saved here to $function
+        $function = $MyInvocation.MyCommand.Name
+
+        # Retrieve all of the URI, method, body, query, result, and success details for the API endpoint
+        Write-Verbose -Message "Gather API Data for $function"
+        $resources = Get-SEPCLoudAPIData -endpoint $function
+        Write-Verbose -Message "Load API data for $($resources.Function)"
+        Write-Verbose -Message "Description: $($resources.Description)"
+    }
+
+    process {
+        $uri = New-URIString -endpoint ($resources.URI) -id $incidentId
+        $uri = Test-QueryParam -querykeys ($resources.Query.Keys) -parameters ((Get-Command $function).Parameters.Values) -uri $uri
+        $body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)
+
+        Write-Verbose -Message "Body is $(ConvertTo-Json -InputObject $body)"
+        $result = Submit-Request -uri $uri -header $script:SEPCloudConnection.header -method $($resources.Method) -body $body
+        $result = Test-ReturnFormat -result $result -location $resources.Result
+        $result = Set-ObjectTypeName -TypeName $resources.ObjectTName -result $result
+
+        return $result
+    }
+}
