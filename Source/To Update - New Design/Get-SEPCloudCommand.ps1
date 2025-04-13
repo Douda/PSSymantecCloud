24e2c4e101692ba5cea4d92a48ecee73573da1c4
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2025-04-13 11:16:49 +0200

    updated Get-SEPCloudCommand with new design - NOT WORKING YET (HTTP 500) - to investigate

--- Source/To Update - New Design/Get-SEPCloudCommand.ps1
+++ Source/To Update - New Design/Get-SEPCloudCommand.ps1
@@ -1,6 +1,6 @@
 function Get-SEPCloudCommand {
 
-    <# TODO write help
+    <#
     .SYNOPSIS
         Gets a list of the SEP Cloud Commands
     .DESCRIPTION
@@ -11,44 +11,37 @@ function Get-SEPCloudCommand {
         Gets a list of the SEP Cloud Commands
     #>
 
+    [CmdletBinding()]
+    Param()
+
     begin {
-        # Init
-        $BaseURL = $($script:configuration.BaseURL)
-        $Token = (Get-SEPCloudToken).Token_Bearer
+        # Check to ensure that a session to the SaaS exists and load the needed header data for authentication
+        Test-SEPCloudConnection | Out-Null
+
+        # API data references the name of the function
+        # For convenience, that name is saved here to $function
+        $function = $MyInvocation.MyCommand.Name
+
+        # Retrieve all of the URI, method, body, query, result, and success details for the API endpoint
+        Write-Verbose -Message "Gather API Data for $function"
+        $resources = Get-SEPCLoudAPIData -endpoint $function
+        Write-Verbose -Message "Load API data for $($resources.Function)"
+        Write-Verbose -Message "Description: $($resources.Description)"
     }
 
     process {
-        $URI = 'https://' + $BaseURL + "/v1/commands/endpoint-search"
-        $allResults = @()
+        # TODO function is not working (500 error response)
+        # TODO tests with PostMan not working either
 
-        do {
-            try {
-                $params = @{
-                    Uri     = $URI
-                    Method  = 'POST'
-                    Body    = @{
-                        query = ""
-                        # next  = 0
-                    }
-                    Headers = @{
-                        Host           = $BaseURL
-                        Accept         = "application/json"
-                        "Content-Type" = "application/json"
-                        Authorization  = $Token
-                    }
-                }
+        $uri = New-URIString -endpoint ($resources.URI) -id $incidentId
+        $uri = Test-QueryParam -querykeys ($resources.Query.Keys) -parameters ((Get-Command $function).Parameters.Values) -uri $uri
+        $body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)
 
-                # $Resp = Invoke-RestMethod @params
-                $Resp = Invoke-SEPCloudWebRequest @params
-                $allResults += $Resp.commands
-                $body.next = $Resp.next
-                # TODO verify the loop works
-            } catch {
-                Write-Warning -Message "Error: $_"
-            }
+        Write-Verbose -Message "Body is $(ConvertTo-Json -InputObject $body)"
+        $result = Submit-Request -uri $uri -header $script:SEPCloudConnection.header -method $($resources.Method) -body $body
+        $result = Test-ReturnFormat -result $result -location $resources.Result
+        $result = Set-ObjectTypeName -TypeName $resources.ObjectTName -result $result
 
-        } until ($null -eq $resp.next)
-
-        return $allResults
+        return $result
     }
 }
